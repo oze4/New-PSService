cbbe9ecd8d2bb9bcb1c82058f46811c5017dead7
Matt Oestreich <matthewpoestreich@gmail.com>
2020-02-11 10:00:26 -0600

    Update Uninstall-ScriptAsService.ps1

--- Source/Functions/Uninstall-ScriptAsService.ps1
+++ Source/Functions/Uninstall-ScriptAsService.ps1
@@ -1,4 +1,4 @@
-﻿function Uninstall-ScriptAsService {
+function Uninstall-ScriptAsService {
     <#
         .SYNOPSIS
          Uninstall a script-as-service binary
@@ -8,6 +8,15 @@
 
         .PARAMETER Name
          The short, terse, unique name of the service - this **CAN NOT** have any spaces in it.
+
+        .PARAMETER ComputerName
+         The name of the computer or computers from which you want to uninstall the script-as-service
+         binary. By default, the command targets the local machine.
+
+        .EXAMPLE
+         Uninstall-ScriptAsService -Name MyProject
+
+         This command will uninstall the script-as-service binary whose service name is `MyProject`.
     #>
     Param(
         [Parameter( Mandatory = $true )]
@@ -16,15 +25,17 @@
     )
 
     Try {
-        $Service = Get-WmiObject -Class win32_service -ComputerName $ComputerName -ErrorAction Stop |
-            Where-Object -FilterScript {$_.Name -eq $Name} -ErrorAction Stop
+        $Service = Get-WmiObject -Class win32_service -ComputerName $ComputerName -ErrorAction Stop `
+        | Where-Object -FilterScript {$_.Name -eq $Name} -ErrorAction Stop
         $null = $Service.StopService()
         $null = $Service.Delete()
     } Catch {
         Throw $_
     }
-    $Service = Get-WmiObject -Class win32_service -ComputerName $ComputerName |
-        Where-Object -FilterScript {$_.Name -eq $Name}
+
+    $Service = Get-WmiObject -Class win32_service -ComputerName $ComputerName `
+    | Where-Object -FilterScript {$_.Name -eq $Name}
+
     If (-not [string]::IsNullOrEmpty($Service)){
         Throw "Service not uninstalled!"
     }
